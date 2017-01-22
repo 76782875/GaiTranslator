15fb4c374fda031708cdbe39076d6835477d4f76
zhengchengyu <912517171@qq.com>
2017-01-22 10:48:43 +0800

    译员端用户中心UI

--- app/src/main/java/com/lyun/lawyer/fragment/TranslatorCenterFragment.java
+++ app/src/main/java/com/lyun/lawyer/fragment/TranslatorCenterFragment.java
@@ -1,8 +1,12 @@
 package com.lyun.lawyer.fragment;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.lyun.fragment.BaseFragment;
+import com.lyun.lawyer.R;
 
 /**
  * @author Gordon
@@ -10,13 +14,20 @@ import com.lyun.fragment.BaseFragment;
  * do(翻译中心页面)
  */
 
-public class TranslatorCenterFragment extends BaseFragment{
+public class TranslatorCenterFragment extends BaseFragment {
     public TranslatorCenterFragment() {
     }
+
     public static TranslatorCenterFragment newInstance() {
         TranslatorCenterFragment fragment = new TranslatorCenterFragment();
         Bundle args = new Bundle();
         fragment.setArguments(args);
         return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_translator_center, container, false);
+        return view;
     }
 }
--- app/src/main/java/com/lyun/lawyer/fragment/TranslatorMainFragment.java
+++ app/src/main/java/com/lyun/lawyer/fragment/TranslatorMainFragment.java
@@ -1,8 +1,12 @@
 package com.lyun.lawyer.fragment;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.lyun.fragment.BaseFragment;
+import com.lyun.lawyer.R;
 
 /**
  * @author Gordon
@@ -13,10 +17,17 @@ import com.lyun.fragment.BaseFragment;
 public class TranslatorMainFragment extends BaseFragment {
     public TranslatorMainFragment() {
     }
+
     public static TranslatorMainFragment newInstance() {
         TranslatorMainFragment fragment = new TranslatorMainFragment();
         Bundle args = new Bundle();
         fragment.setArguments(args);
         return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_translator_main, container, false);
+        return view;
     }
 }
