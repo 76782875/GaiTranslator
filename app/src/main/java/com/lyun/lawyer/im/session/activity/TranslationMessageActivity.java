9aa4e7ee978716ae4418f20f4a52828f4e9909f7
赵尉尉 <4332weizi@gmail.com>
2017-03-21 09:42:52 +0800

    用户时间耗尽结束订单，自动挂断语音通话

--- app/src/main/java/com/lyun/lawyer/im/session/activity/TranslationMessageActivity.java
+++ app/src/main/java/com/lyun/lawyer/im/session/activity/TranslationMessageActivity.java
@@ -292,6 +292,8 @@ public class TranslationMessageActivity extends P2PMessageActivity implements IT
         @Override
         public void onReceive(Context context, Intent intent) {
 
+            hangUpAudioCall(false);
+
             int whoFinish = intent.getIntExtra(TranslationOrder.WHO_FINISH, TranslationOrder.OTHER);
 
             if (whoFinish == TranslationOrder.TRANSLATOR) {
