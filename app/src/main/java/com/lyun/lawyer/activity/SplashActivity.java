f9a7b3c5ae2b22577a7c5450b2bdcd49febd2fff
赵尉尉 <4332weizi@gmail.com>
2017-04-13 11:01:25 +0800

    启动页优化

--- app/src/main/java/com/lyun/lawyer/activity/SplashActivity.java
+++ app/src/main/java/com/lyun/lawyer/activity/SplashActivity.java
@@ -22,29 +22,20 @@ import pub.devrel.easypermissions.EasyPermissions;
  */
 
 public class SplashActivity extends BaseActivity {
-    private static int sleepTime=3500;
+    private static int sleepTime = 3500;
     private Handler mHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        GlideUtils.showImage(this, (ImageView) findViewById(R.id.bg_splash),R.mipmap.bg_splash);
+        GlideUtils.showImage(this, (ImageView) findViewById(R.id.bg_splash), R.mipmap.bg_splash);
 
         checkPermission();
 
         if (mHandler == null)
             mHandler = new Handler();
-        mHandler.postDelayed(() -> {
-            Intent intent = new Intent();
-            if (Account.preference().isLogin()) {
-                intent.setClass(SplashActivity.this, MainActivity.class);
-            } else {
-                intent.setClass(SplashActivity.this, LoginActivity.class);
-            }
-            startActivity(intent);
-            finish();
-        },sleepTime);
+        mHandler.postDelayed(() -> processDone(), sleepTime);
     }
 
     @Override
@@ -57,6 +48,27 @@ public class SplashActivity extends BaseActivity {
         }
     }
 
+    protected boolean canFinish = false;
+
+    protected void processDone() {
+
+        synchronized (this) {
+            if (!canFinish) {
+                canFinish = true;
+                return;
+            }
+        }
+
+        Intent intent = new Intent();
+        if (Account.preference().isLogin()) {
+            intent.setClass(SplashActivity.this, MainActivity.class);
+        } else {
+            intent.setClass(SplashActivity.this, LoginActivity.class);
+        }
+        startActivity(intent);
+        finish();
+    }
+
     protected final int REQUEST_PERMISSION = 0x001;
 
     @AfterPermissionGranted(REQUEST_PERMISSION)
@@ -65,6 +77,7 @@ public class SplashActivity extends BaseActivity {
                 Manifest.permission.RECORD_AUDIO,
                 Manifest.permission.READ_EXTERNAL_STORAGE,
                 Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            processDone();
         } else {
             EasyPermissions.requestPermissions(this, "为保证app正常运行，需要这些权限",
                     REQUEST_PERMISSION,
@@ -78,5 +91,6 @@ public class SplashActivity extends BaseActivity {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+        processDone();
     }
 }
