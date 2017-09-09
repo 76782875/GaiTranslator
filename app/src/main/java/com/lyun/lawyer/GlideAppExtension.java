5685aaf5dbcd84c08cc5991f91002168a88f3a00
赵尉尉 <4332weizi@gmail.com>
2017-09-09 10:55:54 +0800

    更新Glide版本

---
+++ app/src/main/java/com/lyun/lawyer/GlideAppExtension.java
@@ -0,0 +1,11 @@
+package com.lyun.lawyer;
+
+import com.bumptech.glide.annotation.GlideExtension;
+
+@GlideExtension
+public class GlideAppExtension {
+
+    private GlideAppExtension() {
+    }
+
+}
